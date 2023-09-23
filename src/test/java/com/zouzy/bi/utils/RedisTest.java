dcc4912ef72328c1e0955ce14511d87cec6e8a54
zouzy <zouzy@asiainfo.com>
2023-09-23 13:14:57 +0800

    move test classes to another package

---
+++ src/test/java/com/zouzy/bi/utils/RedisTest.java
@@ -0,0 +1,19 @@
+package com.zouzy.bi.utils;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Test
+    public void queryRedis() {
+        RLock k1 = redissonClient.getLock("k1");
+    }
+}
