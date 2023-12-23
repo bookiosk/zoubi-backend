244c37f30375dd3f4aad45048a986f9691c75ca1
zouzhenyu <981592521@qq.com>
2023-12-23 13:19:12 +0800

    改造去除无用的类

--- src/test/java/com/zouzy/bi/MainApplicationTests.java
+++ src/test/java/com/zouzy/bi/MainApplicationTests.java
@@ -4,6 +4,9 @@ import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 主类测试
  *
@@ -13,12 +16,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MainApplicationTests {
 
-    @Resource
-    private WxOpenConfig wxOpenConfig;
 
     @Test
-    void contextLoads() {
-        System.out.println(wxOpenConfig);
-    }
+    void test1 () {
+        Map<Integer,String> map =new HashMap(){{
+            put(1,"zhangsan");
+            put(2,"zhangsan");
+            put(3,"zhangsan");
+        }} ;
 
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            map.put(1,"jjj"); //这里会报错么
+            System.out.printf("key:%d,value:%s \n",entry.getKey(),entry.getValue());
+        }
+    }
 }
